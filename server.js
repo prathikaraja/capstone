b59913ef2af93f4496ead971816d48efe43a5f28
prathikaraja <prathikaraja26@gmail.com>
2026-09-23 21:36:08 +0530

    Day 3 and Day 4 updates: backend setup and client proxy

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const sequelize = require('./config/db');
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend Running Successfully 🚀");
-});
-
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+sequelize.sync({ alter: true })
+  .then(() => console.log('Database & tables connected!'))
+  .catch((err) => console.error('Database connection error:', err));
